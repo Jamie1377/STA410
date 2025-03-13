78417f459f0d7cad708286191e5e6eb56639e7ce
Jamie <jamie@Jamies-MacBook-Air-9.local>
2025-03-13 15:31:25 -0400

    Write the code in usage as a function

--- stock_prediction/docs/example_usage.py
+++ stock_prediction/docs/example_usage.py
@@ -1,35 +1,5 @@
-from stock_prediction.core import StockPredictor, ARIMAXGBoost
-from datetime import date, timedelta
-import matplotlib.pyplot as plt
-import time
-import pandas as pd
-# # Initialize predictor
-# predictor = StockPredictor("AAPL", "2024-01-01")
-# predictor.load_data()
-
-# # Custom model configuration
-# model_config = {
-#     "use_sarima": True,
-#     "use_stacking": True,
-#     "use_lgbm": True,
-#     "weights": {"sarima": 0.6, "stacking": 0.4},
-#     "sarima_order": (1, 1, 1),
-#     "seasonal_order": (1, 1, 1, 5),
-# }
-
-# # Train and predict
-# predictor.train_model(model_config)
-# forecast = predictor.predict(days=5)
-
-# print("Next 5 day forecast:")
-# print(forecast)
-
-
-
-# for company in list(finance.top_companies.index):
-# for company in list(tech.top_etfs.keys()):
-# for company in list(tech.top_companies.iloc[30:45,].index):
-
+from stock_prediction.core import StockPredictor
+from datetime import date
 stock_settings = {
     "V": {"horizons": [7], "weight": False},
     "GE": {"horizons": [5], "weight": False},
@@ -53,146 +23,102 @@ stock_settings = {
     "ASML": {"horizons": [10], "weight": False},
     "MTCH": {"horizons": [10], "weight": False},}
 
-# Default settings f1or other companies
-default_horizons = [5, 7, 10]
-default_weight = False
- 
-for company in [
-    # [random.choice(Tickers["stock"]) if random.choice(Tickers["stock"]) not in stock_settings.keys() else ValueError][0],
-    # [random.choice(Tickers["stock"]) if random.choice(Tickers["stock"]) not in stock_settings.keys() else ValueError][0],
-    # [random.choice(Tickers["stock"]) if random.choice(Tickers["stock"]) not in stock_settings.keys() else ValueError][0],
-    # 'ZS',
-    # 'SBUX',
-    # "LCID", #shit
-    # "IBKR",
-    # "AFL",
-    # "MET",
-    # "AAPL",
-    # "ORCL",
-    # # "AMD",
-    # "ANF",  # not good for day ahead
-    # "AVGO", # ok day ahead try True
-    # "AXP", # not good for day ahead try True
-    # "V",
-    # "MCO",
-    # "GE",
-    # "NVDA",
-    # "PYPL",
-    # "CRWD", # watch
-    # "SPGI", # not good for day ahead
-    # "EBAY",
-    # "KDP",
-    # "MET",
-    "MTCH"
-]:
-    # [
-    #     "MET",
-    #     "AVGO",
-    #     "SPGI",
-    #     "AIG",
-    #     "AXP",
-        # "MCO",
-    #     "PYPL",
-    #     "V",
-    #     "MS"]:
 
-    # [
-    #     "V",
-    #     "MS",
-    #     "GS",
-    #     "AXP",
-    #     "BX",
-    #     "BLK",
-    #     "SPGI",
-    #     "MCO",
-    #     "PYPL",
-    #     "IBKR",
-    #     "AFL",
-    #     "MET",
-    #     "ARES",
-    #     "NU",
-    #     "AIG",
-    #     "PRU",
-    # ]:
-  
-    prediction_dataset = StockPredictor(
-        company,
-        start_date="2023-01-01",
-        end_date= date.today(),
-        interval="1d",
-        #  + pd.Timedelta(days=1)
-    )
-    prediction_dataset.load_data()
-# Volume mau not help stock price prediction
-   
-    predictors =  ['Close', 'MA_50', 'MA_200',
-                    # 'BB_Low',
-                    'SP500','TNX','USDCAD=X','Tech','Fin','VIX'] + ['rolling_min', 'rolling_median',
-       'rolling_sum', 'rolling_ema', 'rolling_25p','rolling_75p',] 
-    
-    predictor = prediction_dataset
-    if company in stock_settings:
-        # Use custom settings for the stock
-        settings = stock_settings[company]
-        horizons = settings["horizons"]
-        weight = settings["weight"]
-    else:
-        # Use default settings for other stocks
-        horizons = default_horizons
-        weight = default_weight
-    for horizon in horizons: #[5, 7, 10, 12]:  # [3, 5, 7]:,
-        prediction_dataset.prepare_models(predictors, horizon=horizon, weight=weight)
-        # prediction_dataset._evaluate_models('Close')
-        prediction, backtest, predictions_dict = predictor.one_step_forward_forecast(
-            predictors, model_type="arimaxgb", horizon=horizon
-        )
 
-        # Data Viz (Not that key)
-        plt.figure(figsize=(12, 6))
 
-        first_day = pd.to_datetime(date.today() - timedelta(days=10 + horizon))
 
-        plt.plot(
-            prediction[prediction.index >= prediction_dataset.data.iloc[-1].name].index,
-            prediction[prediction.index >= prediction_dataset.data.iloc[-1].name].Close,
-            label="Prediction",
-            color="blue",
-        )
-        plt.plot(
-            backtest[backtest.index > first_day].index,
-            backtest[backtest.index > first_day].Close,
-            label="Backtest",
-            color="red",
-        )
-        plt.plot(
-            prediction_dataset.data.Close[
-                prediction_dataset.data.index > first_day
-            ].index,
-            prediction_dataset.data.Close[prediction_dataset.data.index > first_day],
-            label="Actual",
-            color="black",
-        )
-        # cursor(hover=True)
 
-        current_time = time.strftime("%H:%M:%S", time.localtime())
-        plt.title(
-            f"Price Prediction ({prediction_dataset.symbol}) (horizon = {horizon}) (weight = {weight})"
-        )
-        plt.axvline(
-            x=backtest.index[-1],
-            color="g",
-            linestyle="--",
-            label="Reference Line (Last Real Data Point)",
-        )
-        plt.text(
-            backtest.index[-1],
-            backtest.Close[-1],
-            f"x={str(backtest.index[-1].date())}",
-            ha="right",
-            va="bottom",
-        )
+# def full_workflow(start_date, end_date, predictors = None, companies = None, ):
+#     """
+#     This function is used to output the prediction of the stock price for the next 10 days
 
-        plt.xlabel("Date")
-        plt.ylabel("Stock Price")
-        plt.legend()
-        plt.show()
+#     Args:
+#     start_date (str): The start date of the stock price data
+#     end_date (str): The end date of the stock price data
+#     predictors (list): The list of predictors used to predict the stock price
+#     companies (list): The list of company names of the stocks
+#     """
+#     default_horizons = [5, 7, 10]
+#     default_weight = False
+#     if companies is None:
+#         companies = ["AXP"]
+#     for company in companies:
+#         prediction_dataset = StockPredictor(
+#             company,
+#             start_date=start_date,
+#             end_date= end_date,
+#             interval="1d",
+#         )
+#         prediction_dataset.load_data()
+#         if predictors is None:
+#             predictors =  ['Close', 'MA_50', 'MA_200',
+#                         'SP500','TNX','USDCAD=X','Tech','Fin','VIX'] + ['rolling_min', 'rolling_median',
+#         'rolling_sum', 'rolling_ema', 'rolling_25p','rolling_75p',] 
+#         predictors = predictors
+        
+#         predictor = prediction_dataset
+#         if company in stock_settings:
+#             # Use custom settings for the stock
+#             settings = stock_settings[company]
+#             horizons = settings["horizons"]
+#             weight = settings["weight"]
+#         else:
+#             # Use default settings for other stocks
+#             horizons = default_horizons
+#             weight = default_weight
+#         for horizon in horizons: 
+#             prediction_dataset.prepare_models(predictors, horizon=horizon, weight=weight)
+#             # prediction_dataset._evaluate_models('Close')
+#             prediction, backtest, predictions_dict = predictor.one_step_forward_forecast(
+#                 predictors, model_type="arimaxgb", horizon=horizon
+#             )
+
+#             # Data Viz (Not that key)
+#             plt.figure(figsize=(12, 6))
+
+#             first_day = pd.to_datetime(end_date - timedelta(days=10 + horizon))
+
+#             plt.plot(
+#                 prediction[prediction.index >= prediction_dataset.data.iloc[-1].name].index,
+#                 prediction[prediction.index >= prediction_dataset.data.iloc[-1].name].Close,
+#                 label="Prediction",
+#                 color="blue",
+#             )
+#             plt.plot(
+#                 backtest[backtest.index > first_day].index,
+#                 backtest[backtest.index > first_day].Close,
+#                 label="Backtest",
+#                 color="red",
+#             )
+#             plt.plot(
+#                 prediction_dataset.data.Close[
+#                     prediction_dataset.data.index > first_day
+#                 ].index,
+#                 prediction_dataset.data.Close[prediction_dataset.data.index > first_day],
+#                 label="Actual",
+#                 color="black",
+#             )
+#             # cursor(hover=True)
+#             plt.title(
+#                 f"Price Prediction ({prediction_dataset.symbol}) (horizon = {horizon}) (weight = {weight})"
+#             )
+#             plt.axvline(
+#                 x=backtest.index[-1],
+#                 color="g",
+#                 linestyle="--",
+#                 label="Reference Line (Last Real Data Point)",
+#             )
+#             plt.text(
+#                 backtest.index[-1],
+#                 backtest.Close[-1],
+#                 f"x={str(backtest.index[-1].date())}",
+#                 ha="right",
+#                 va="bottom",
+#             )
+
+#             plt.xlabel("Date")
+#             plt.ylabel("Stock Price")
+#             plt.legend()
+#             plt.show()
+
+StockPredictor.full_workflow('2023-01-01',date.today())

c670031abdbc1f128126a4e6aa8e6eba92e901c1
Jamie <jamie@Jamies-MacBook-Air-9.local>
2025-04-02 18:00:24 -0400

    feat: take care of the consistency issue and hyperparameter (rmsprop, velocity=0.9) tuning

--- stock_prediction/docs/test_new_algo.py
+++ stock_prediction/docs/test_new_algo.py
@@ -5,10 +5,15 @@ from sklearn.model_selection import train_test_split
 stock = StockPredictor("NVDA", "2024-01-01")
 stock.load_data()
 
-X_train, X_test, y_train, y_test = train_test_split(
-    stock.data.drop(columns="Close"), stock.data["Close"], shuffle=False
-)
-print(X_test.index)
+
+X =  stock.data.drop(columns="Close")
+y = stock.data["Close"]
+train_pct_index = int(0.8 * len(stock.data))
+X_train, X_test = X[:train_pct_index], X[train_pct_index:]
+y_train, y_test = y[:train_pct_index], y[train_pct_index:]
+
+# print(X_test.index)
+model = None
 model = ARIMAXGBoost()
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
